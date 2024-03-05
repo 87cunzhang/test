8771ed486f3069731dacd649d24126e8f14cfcfc
87cunzhang <2423561050@qq.com>
2024-03-05 11:41:04 +0800

    1次

--- index.php
+++ index.php
@@ -22,8 +22,4 @@ function quickSort($arr)
 
 }
 
-quickSort([3, 2, 1, 5, 4, 6, 7, 8, 9, 0]);
-quickSort([3, 2, 1, 5, 4, 6, 7, 8, 9, 0]);
-quickSort([3, 2, 1, 5, 4, 6, 7, 8, 9, 0]);
-quickSort([3, 2, 1, 5, 4, 6, 7, 8, 9, 0]);
-quickSort([3, 2, 1, 5, 4, 6, 7, 8, 9, 0]);
+quickSort([0]);
